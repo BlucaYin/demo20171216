91f9e05fe7f84b883ad87e5bd1017375c0357cf1
Black <yinshilong66@126.com>
2017-12-20 09:47:02 +0800

    Initial commit

---
+++ src/com/ysl/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.ysl.demo;
+
+/**
+ * demo
+ *
+ * @author Administrator
+ * @create 2017-12-16 16:34
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
